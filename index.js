a86e4773c62228350033e68e8443530d21683768
Benson <bensonmakau2000@gmail.com>
2023-08-31 09:44:09 +0300

    first test

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ inquirer
     /* Pass your questions in here */
     {
         type: 'input',
-        name: 'name', 
+        name: 'input_type', 
         message: 'What is your name?',
         default:'Super Admin'
+    },
+    {
+        type: 'list',
+        name: 'list question', 
+        message: 'What programming language do you like?',
+        choices:['JavaScript', 'C++','Java','Python'],
+        default:'JavaScript'
+    },
+    {
+        type: 'checkbox',
+        name: 'checkbox question', 
+        message: 'How many programming languages do you speak?',
+        choices:['JavaScript', 'C++','Java','Python'],
+        default:'JavaScript'
     },
   ])
   .then((answers) => {
